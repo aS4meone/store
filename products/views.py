8a814d87c8dfb89cf42c8e30fe7416b3abf9fa14
aS4meone <108177472+aS4meone@users.noreply.github.com>
2023-04-28 20:19:02 +0600

    basket

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
 
 
 def index(request):
@@ -17,3 +17,23 @@ def products(request):
         'products': Product.objects.all(),
     }
     return render(request, 'products/products.html', context)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def basket_delete(request, id):
+    basket = Basket.objects.get(id=id)
+    basket.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from products.models import Basket
 
 
 def login(request):
@@ -37,13 +39,27 @@ def register(request):
 
 
 def profile(request):
+    user = request.user
     if request.method == 'POST':
         form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('users:profile'))
     else:
-        form = UserProfileForm(instance=request.user)
+        form = UserProfileForm(instance=user)
 
-    context = {'form': form}
+    baskets = Basket.objects.filter(user=user)
+    total_quantity = sum(basket.quantity for basket in baskets)
+    total_sum = sum(basket.sum() for basket in baskets)
+
+    context = {'form': form,
+               'baskets': Basket.objects.filter(user=user),
+               'total_quantity': total_quantity,
+               'total_sum': total_sum,
+               }
     return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
